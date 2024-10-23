cabf04dcf7c9d7ca85d8890a116e11e10882fabc
DeveloperMan313 <053000voloshch@gmail.com>
2024-10-24 01:02:03 +0300

    Convert pages to templates with placeholder data

--- app/views.py
+++ app/views.py
@@ -1,3 +1,74 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Title {i}',
+        'text': 'text ' * i,
+        'answer_count': i,
+        'tags': [f'tag{j}' for j in range(1, i + 1)],
+        'likes': i,
+    }
+    for i in range(1, 5)
+]
+
+ANSWERS = [
+    {
+        'id': i,
+        'text': 'text ' * i,
+        'likes': i,
+        'is_correct': i == 1,
+    }
+    for i in range(1, 5)
+]
+
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+        context={
+            'page_title': 'AskPupkin',
+            'is_logged_in': True,
+            'questions': QUESTIONS,
+            'category': 'new',
+        },
+    )
+
+
+def hot(request):
+    return render(
+        request,
+        'index.html',
+        context={
+            'page_title': 'AskPupkin - Hot',
+            'is_logged_in': True,
+            'questions': reversed(QUESTIONS),
+            'category': 'top',
+        },
+    )
+
+
+def ask(request):
+    return render(
+        request,
+        'ask.html',
+        context={
+            'page_title': 'AskPupkin - Ask',
+            'is_logged_in': True,
+        },
+    )
+
+
+def question(request, question_id):
+    return render(
+        request,
+        'question.html',
+        context={
+            'page_title': 'AskPupkin - Question',
+            'is_logged_in': True,
+            'question': next(filter(lambda q: q['id'] == question_id, QUESTIONS)),
+            'answers': ANSWERS,
+        },
+    )
--- ask_pupkin/urls.py
+++ ask_pupkin/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('ask/', views.ask, name='ask'),
+    path('question/<int:question_id>/', views.question, name='question'),
     path('admin/', admin.site.urls),
 ]
