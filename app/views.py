952ccedb3f521a449c85f13bacc3e645975f2878
DeveloperMan313 <053000voloshch@gmail.com>
2024-10-30 21:11:33 +0300

    Add tag, settings, login and signup pages, add question pagination, fill in URLs, misc changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 
 
@@ -10,7 +11,7 @@ QUESTIONS = [
         'tags': [f'tag{j}' for j in range(1, i + 1)],
         'likes': i,
     }
-    for i in range(1, 5)
+    for i in range(1, 23)
 ]
 
 ANSWERS = [
@@ -23,29 +24,77 @@ ANSWERS = [
     for i in range(1, 5)
 ]
 
+POPULAR_TAGS = [f'tag{i}' for i in range(1, 10)]
+
+BEST_MEMBERS = [f'member{i}' for i in range(1, 4)]
+
+
+def paginate(objects_list, request, per_page=5):
+    try:
+        page_num = int(request.GET['p'])
+    except KeyError:
+        page_num = 1
+    except ValueError:
+        page_num = 1
+    num_pages = ceil(len(objects_list) / per_page)
+    page_num = max(1, min(page_num, num_pages))
+    page = objects_list[(page_num - 1) * per_page:page_num * per_page]
+    return page_num, page, num_pages
+
 
 def index(request):
+    page_num, questions, num_pages = paginate(QUESTIONS, request)
     return render(
         request,
         'index.html',
         context={
             'page_title': 'AskPupkin',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'page': page_num,
+            'num_pages': num_pages,
             'is_logged_in': True,
-            'questions': QUESTIONS,
+            'questions': questions,
             'category': 'new',
         },
     )
 
 
 def hot(request):
+    page_num, questions, num_pages = paginate(
+        tuple(reversed(QUESTIONS)), request)
     return render(
         request,
         'index.html',
         context={
             'page_title': 'AskPupkin - Hot',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'page': page_num,
+            'num_pages': num_pages,
             'is_logged_in': True,
-            'questions': reversed(QUESTIONS),
+            'questions': questions,
             'category': 'top',
+        },
+    )
+
+
+def tag(request, tag):
+    page_num, questions, num_pages = paginate(
+        tuple(filter(lambda q: tag in q['tags'], QUESTIONS)),
+        request)
+    return render(
+        request,
+        'tag.html',
+        context={
+            'page_title': f'AskPupkin - Tag: {tag}',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'page': page_num,
+            'num_pages': num_pages,
+            'is_logged_in': True,
+            'tag': tag,
+            'questions': questions,
         },
     )
 
@@ -56,6 +105,8 @@ def ask(request):
         'ask.html',
         context={
             'page_title': 'AskPupkin - Ask',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
             'is_logged_in': True,
         },
     )
@@ -67,8 +118,49 @@ def question(request, question_id):
         'question.html',
         context={
             'page_title': 'AskPupkin - Question',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
             'is_logged_in': True,
             'question': next(filter(lambda q: q['id'] == question_id, QUESTIONS)),
             'answers': ANSWERS,
+        },
+    )
+
+
+def settings(request):
+    return render(
+        request,
+        'settings.html',
+        context={
+            'page_title': 'AskPupkin - Settings',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'is_logged_in': True,
+        },
+    )
+
+
+def signup(request):
+    return render(
+        request,
+        'signup.html',
+        context={
+            'page_title': 'AskPupkin - Sign Up',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'is_logged_in': False,
+        },
+    )
+
+
+def login(request):
+    return render(
+        request,
+        'login.html',
+        context={
+            'page_title': 'AskPupkin - Log In',
+            'popular_tags': POPULAR_TAGS,
+            'best_members': BEST_MEMBERS,
+            'is_logged_in': False,
         },
     )
--- ask_pupkin/urls.py
+++ ask_pupkin/urls.py
@@ -23,7 +23,11 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hot/', views.hot, name='hot'),
+    path('tag/<str:tag>/', views.tag, name='tag'),
     path('ask/', views.ask, name='ask'),
     path('question/<int:question_id>/', views.question, name='question'),
+    path('settings/', views.settings, name='settings'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('admin/', admin.site.urls),
 ]
